586b17282da40bb24c4b7a43550ac9d662759e85
dallli92 <dallli92@live.fr>
2019-06-08 18:55:07 +0200

    skill activity

--- app/src/main/java/com/dalilandoulsi/basketme/LeagueActivity.kt
+++ app/src/main/java/com/dalilandoulsi/basketme/LeagueActivity.kt
@@ -1,12 +1,20 @@
 package com.dalilandoulsi.basketme
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun leagueNextClicked(view: View) {
+
+        val intent = Intent(this, SkillActivity::class.java)
+        startActivity(intent)
     }
 }
